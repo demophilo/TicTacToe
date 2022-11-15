bb457c9b4f568de3d63b5844ae947e9181e554b7
Karl Linek <karl@linek.at>
2022-11-15 20:51:58 +0100

    new functions intro, input players

--- menu.js
+++ menu.js
@@ -1,73 +1,50 @@
 const prompt = require("prompt-sync")();
 
 //*************first menu on display********
+function intro () {
+  console.log("Welcome to TicTacToe");
+  console.log("\n" + "Please enter s to start the game.");
+  console.log("\n\n" + "You can quit the game by entering quit.");
 
-console.log("welcome to TicTacToe");
-console.log("\n" + "Please enter s to start the game.");
-console.log("\n\n" + "You can quit the game by entering quit.");
+  let yourChoice = "Your choice: ";
+  let userStartInput = prompt(yourChoice).toLowerCase();
 
-let yourChoice = "Your choice: ";
-let userStartInput = prompt(yourChoice).toLowerCase();
+  if (userStartInput === "s") {
+    displayMenu();
+  } else if (userStartInput === "quit") {
+    endGame();
+  } else {
+    console.log("\n" + "Please enter s to start Tic Tac Toe");
+  }
 
-if (userStartInput === "s") {
-  displayMenu();
-} else if (userStartInput === "quit") {
-  endGame();
-} else {
-  console.log("\n" + "Please enter just s for Start Game");
 }
 
 //*************DISPLAY MENU****************
-console.log("TIC TAC TOE Menu");
-function displayMenu() {
+
+function inputPlayers() {
   let players = [];
-  let player = {"name":"", "id":};
   for (let playerId = 1; playerId <=2; playerId++) {
-    player.prompt(`Who is player ${playerId}?`);
+    console.log(`Who is player ${playerId}?`);
+    console.log("Type in your name or chose an AI.")
+    console.log("List of AI's:\nBabybot\nMoronbot\nGolem XIV\nHonest Annie")
+    let nameInput = prompt();
+    const player = {"name": nameInput, "playerId": playerId}
+  }
 }
 
-//************userChoice************
-function getUserChoice() {
-  let userSecondInput = prompt(yourChoice).toLowerCase();
-  if (userSecondInput === 1) {
-    console.log("human vs Human");
-  }
-  if (userSecondInput === 2) {
-    console.log("Random AI vs Random AI");
-  }
-  if (userSecondInput === 3) {
-    console.log("Human vs Random AI");
-  }
-  if (userSecondInput === 4) {
-    console.log("Human vs unbeatable AI");
-  }
-  if (userSecondInput !== 0 || 1 || 2 || 3 || 4) {
-    console.log("please choose one of the choices between 1 or 2 or 3 or 4");
-  }
-  return userSecondInput;
-}
+
 
 //*********endGame******** */
 function endGame() {
-  return alert("END");
+   console.log("Game is over, as you wish master");
 }
 
-function getMenuOption() {
-  console.log("TicTacToe");
-  console.log("\n");
-  let UserChoice = prompt(yourChoice);
-  return UserChoice;
-}
 
+/*
 module.exports = {
   getMenuOption: getMenuOption,
   getUserChoice: getUserChoice,
 };
 
-// run this function to test whether you have correctly implemented the other function
-function checkOptions() {
-  let option = getMenuOption();
-  console.log("If the user selected 1, it should print 1");
-  console.log(option);
-}
-checkOptions();
+
+*/
